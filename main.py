496d06b06822680a4825437d1e7b4d63108883b5
Lovely Sharma <lovelysingeras297@gmail.com>
2022-03-07 19:28:36 +0530

    created empty lists to store data

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ import pandas as pd
 import numpy as np
 import xlrd as xd
 
+#reading the input excel file
 data=pd.read_excel('input_1.xlsx')
+
+#getting the size of sheet rows and columns
 rows=int(data.shape[0])
-cols=data.shape[1]
+cols=int(data.shape[1])
+
+#to append values for each candidate
+name=[]
+username=[]
+ch_tag=[]   
+test_name=[]
+answered=[]
+correct=[]
+score=[]
+skipped=[]
+time_taken=[]
+wrong=[]
+
